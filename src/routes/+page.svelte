215cb4860727f69f547d8e3d0e824465647b8c10
harshascode <harshamesta0088@gmail.com>
2024-12-24 21:54:20 +0530

    update 7777

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
 		>
 			恋みくじ
 		</h1>
-		<p class="text-2xl font-medium tracking-wide text-gray-700">運命の恋を占う88888888</p>
+		<p class="text-2xl font-medium tracking-wide text-gray-700">運命の恋を占う7777777</p>
 	</header>
 
 	<!-- Koi Image Section -->
